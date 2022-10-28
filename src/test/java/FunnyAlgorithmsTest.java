489bc2e5cb36b93b8b34721df8e1aedb3b151461
Emanuele Conforti <manu.conforti@gmail.com>
2022-10-28 17:25:30 +0200

    Test 6 and Joda-time

--- src/test/java/FunnyAlgorithmsTest.java
+++ src/test/java/FunnyAlgorithmsTest.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.*;
+
+import org.joda.time.LocalDateTime;
 import org.junit.*;
 import Application.FunnyAlgorithms;
 
@@ -29,26 +31,26 @@ public class FunnyAlgorithmsTest {
 	
 	@Test
 	public void numberTest() {  // test 1
-		System.out.println("Test with number 123");
+		System.out.println(LocalDateTime.now() + " --- " + "Test with number 123");
 		assertEquals(123, f.stringToIntConverter("123"));
 	}
 	
 	@Test(expected = IllegalArgumentException.class)  // test 2
 	public void numberInRangeTest() {
-		System.out.println("Test of number in range [-32768, 32767]");
+		System.out.println(LocalDateTime.now() + " --- " + "Test of number in range [-32768, 32767]");
 		f.stringToIntConverter("32768");
 	}
 	
 	@Test(expected = IllegalArgumentException.class)  // test 3
 	public void illegalNumberTest() {
-		System.out.println("Test of illegal number (like A3, 2.3, 2 3, ...)");
+		System.out.println(LocalDateTime.now() + " --- " + "Test of illegal number (like A3, 2.3, 2 3, ...)");
 		f.stringToIntConverter("A3");
 	}
 	
 	@Test(expected = IllegalArgumentException.class)  // test 4
 	public void illegalOrderTest() {
 		int array[] = {1,2,3,4,5}; 
-		System.out.println("Test of order != 0 and order != 1 in selection sort");
+		System.out.println(LocalDateTime.now() + " --- " + "Test of order != 0 and order != 1 in selection sort");
 		f.selectionSort(array, 3);
 	}
 	
@@ -57,8 +59,15 @@ public class FunnyAlgorithmsTest {
 		int size = 6;
 		int array[] =  {4,3,5,1,2,6};
 		int orderedArray[] = {1,2,3,4,5,6}; 
-		System.out.println("array [4,3,5,1,2,6] is ordered");
+		System.out.println(LocalDateTime.now() + " --- " + "array [4,3,5,1,2,6] is ordered");
 		f.selectionSort(array, 0);
 		assertArrayEquals(orderedArray, array);
+	}
+	
+	@Test
+	public void binarySearchTest() {  // test 6: binary search sbagliata... per mancanza di tempo non ho risolto
+		int array [] = {1,2,3,4,5,6,7,8,9,10};
+		System.out.println(LocalDateTime.now() + " --- " + "Test of binary search");
+		assertEquals(0, f.binarySearch(array, 0));
 	}
 }
