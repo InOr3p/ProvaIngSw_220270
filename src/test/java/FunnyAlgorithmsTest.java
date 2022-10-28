c39bc59839cdc3c1d25c9cd9deb2bbf1f2e396f8
Emanuele Conforti <manu.conforti@gmail.com>
2022-10-28 16:32:57 +0200

    Test 1

---
+++ src/test/java/FunnyAlgorithmsTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import Application.FunnyAlgorithms;
+
+public class FunnyAlgorithmsTest {
+	
+	private FunnyAlgorithms f = new FunnyAlgorithms();
+	
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("before class");
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("after class");
+	}
+	
+	@Before
+	public void beforeTest() {
+		System.out.println("before test");
+	}
+	
+	@After
+	public void afterTest() {
+		System.out.println("after test");
+	}
+	
+	@Test
+	public void numberTest() {
+		System.out.println("Test with number 123");
+		assertEquals(123, f.stringToIntConverter("123"));
+	}
+}
