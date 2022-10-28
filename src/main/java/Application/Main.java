446126e5db5d20d631b5e9580018aa5ae5339028
Emanuele Conforti <manu.conforti@gmail.com>
2022-10-28 16:06:30 +0200

    First commit

---
+++ src/main/java/Application/Main.java
@@ -0,0 +1,9 @@
+package Application;
+
+public class Main {
+	public static void main(String[] args) {
+		FunnyAlgorithms f = new FunnyAlgorithms();
+		System.out.println("Il numero è: ");
+		System.out.println(f.stringToIntConverter("123"));
+	}
+}
